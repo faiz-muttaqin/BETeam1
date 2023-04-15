27428b92db31fbe4a84396bc58d22d23394f9460
Faiz Muttaqin <38288096+faiz-muttaqin@users.noreply.github.com>
2023-04-15 11:28:39 +0700

    try deploy to netlify

---
+++ build.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const expressStatic = require('express-static');
+
+const outputPath = path.join(__dirname, 'public');
+const serverFile = path.join(__dirname, 'server.js');
+
+expressStatic(serverFile, outputPath, (err) => {
+  if (err) {
+    console.error('Error during the build:', err);
+    process.exit(1);
+  } else {
+    console.log('Static files have been generated in the "public" folder.');
+  }
+});
